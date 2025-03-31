76a30a0956be22cc39a93404dd6bb01f4c4ed717
vikashnitk18 <vikashnifft25@gmail.com>
2025-03-31 22:27:08 +0530

    update

--- firebase_config/firestore.py
+++ firebase_config/firestore.py
@@ -1,5 +1,4 @@
 from .config import db
-from users.models import User
 
 def get_user_email(uid):
     """
@@ -10,30 +9,3 @@ def get_user_email(uid):
     if doc.exists:
         return doc.to_dict().get('email')
     return None
-
-def listen_for_user_deletions():
-    print("Initializing Firestore listener...")
-
-    def on_snapshot(col_snapshot, changes, read_time):
-        try:
-            print("Snapshot received.")
-            for change in changes:
-                print(f"Change detected: {change.type.name}")
-                if change.type.name == 'REMOVED':
-                    uid = change.document.id
-                    print(f"Document with UID {uid} was removed.")
-                    try:
-                        user = User.objects.get(uid=uid)
-                        user.delete()
-                        print(f"User with UID {uid} deleted from Django database.")
-                    except User.DoesNotExist:
-                        print(f"User with UID {uid} does not exist in Django database.")
-        except Exception as e:
-            print(f"Error in Firestore listener: {e}")
-
-    try:
-        users_collection = db.collection('users')
-        users_collection.on_snapshot(on_snapshot)
-        print("Firestore listener started successfully.")
-    except Exception as e:
-        print(f"Error initializing Firestore listener: {e}")
--- users/apps.py
+++ users/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users'
-
-    def ready(self):
-        from firebase_config.firestore import listen_for_user_deletions
-        listen_for_user_deletions()
