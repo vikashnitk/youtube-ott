a5e6fdbfb156b6eb83f0bd09c3442a49caba7255
vikashnitk18 <vikashnifft25@gmail.com>
2025-04-13 22:26:37 +0530

    updated

--- coninue_watching/models.py
+++ coninue_watching/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from users.models import User  # Reference the User model
+from api.models import Movie, Episode  # Import Movie and Episode models
 
-# Create your models here.
+class ContinueWatchingViewHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Reference the User model
+    movie = models.ForeignKey(Movie, null=True, blank=True, on_delete=models.CASCADE)  # Optional ForeignKey to Movie
+    episode = models.ForeignKey(Episode, null=True, blank=True, on_delete=models.CASCADE)  # Optional ForeignKey to Episode
+    play_time = models.IntegerField()  # Store play time in seconds
+    timestamp = models.DateTimeField(auto_now=True)  # Timestamp when the data was received
+
+    def __str__(self):
+        if self.movie:
+            return f"User: {self.user.email}, Movie: {self.movie.title}, Time: {self.play_time}s"
+        if self.episode:
+            return f"User: {self.user.email}, Episode: {self.episode.title}, Time: {self.play_time}s"
+        return f"User: {self.user.email}, No video, Time: {self.play_time}s"
