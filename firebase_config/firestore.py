d4a722769028e50d08910f63a1e0468590fe47e4
vikashnitk18 <vikashnifft25@gmail.com>
2025-05-25 11:44:39 +0530

    update

--- firebase_config/firestore.py
+++ firebase_config/firestore.py
@@ -4,14 +4,14 @@ def get_user_email(uid):
     """
     Retrieves the email of the user from Firestore using the UID.
     """
-    doc_ref = db.collection('users').document(uid)
+    doc_ref = db.collection('User').document(uid)
     doc = doc_ref.get()
     if doc.exists:
         return doc.to_dict().get('email')
     return None
 
 def get_user_details(uid):
-    doc = db.collection('users').document(uid).get()
+    doc = db.collection('User').document(uid).get()
     if doc.exists:
         return doc.to_dict()
     return None
