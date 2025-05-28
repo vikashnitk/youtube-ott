ba419348913a923637603620c59053b3fa0f9555
vikashnitk18 <vikashnifft25@gmail.com>
2025-05-28 12:25:22 +0530

    Add user age retrieval in MovieModelList queryset

--- api/views.py
+++ api/views.py
@@ -11,6 +11,7 @@ class MovieModelList(generics.ListAPIView):
     def get_queryset(self):
         title = self.kwargs.get('title', '').strip()
         user = self.request.user
+        print(f"User: {user}")
         if user.is_authenticated and hasattr(user, 'age'):
             user_age = user.age
         else:
