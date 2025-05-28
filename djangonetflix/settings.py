0956239160d583139ff2bcf3b8dbc9dfe746b7d1
vikashnitk18 <vikashnifft25@gmail.com>
2025-05-28 13:06:33 +0530

    Update REST_FRAMEWORK settings to use Firebase authentication

--- djangonetflix/settings.py
+++ djangonetflix/settings.py
@@ -196,10 +196,20 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# REST_FRAMEWORK = {
+#     'DEFAULT_RENDERER_CLASSES':['rest_framework.renderers.JSONRenderer',
+#                                 'rest_framework.authentication.TokenAuthentication',]
+# }
+
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES':['rest_framework.renderers.JSONRenderer',
-                                'rest_framework.authentication.TokenAuthentication',]
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'firebase_config.auth.FirebaseAuthentication',  # Use your custom class here
+    ],
 }
+
 # REST_FRAMEWORK = {
 #     'DEFAULT_AUTHENTICATION_CLASSES': [
 #         'rest_framework.authentication.TokenAuthentication',
